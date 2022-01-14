c2a86a9c28acc51053cc3b1f34a4c29734935973
Michal Simik <michal.simik@gmail.com>
2022-01-14 13:06:13 +0100

    feat: add social media buttons

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="main" class="container-fluid m-0 p-0">
     <div id="home-page" class="bg-danger p-4">
-      <div id="right-side">
+      <div id="left-side">
         <h1>NFTerra</h1>
         <p>An NFT-based game that connects digital art<br>with real places and allows players to explore it.</p>
-        <button>lightpaper</button>
-        <div>
-          <button>discord</button>
-          <button>telegram</button>
+        <button id="lightpaper">LIGHTPAPER</button>
+        <div id="socials">
+          <button id="discord" onclick="goToDiscord()"/>
+          <button id="telegram"/>
         </div>
       </div>
-      <div id="left-side">
+      <div id="right-side">
         <img id="main-image" src="../assets/main.png">
-
       </div>
     </div>
     <div id="footer" class="bg-warning p-4">
@@ -31,32 +30,82 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #left-side {
-    height: 100vh;
+
+  h1 {
+    font-size: 62px;
   }
+  
+  p {
+    font-size: 20px;
+  }
+
   #main {
     margin: auto;
-  }
-
-  img {
-    max-width: 100%;
-    max-height: 100%;
-  }
-
-  #right-side {
-      float: left;
-  width: 50%;
-    
-  }
-
-  #footer {
   }
 
   #home-page {
     display: flex;
     justify-content: center;
-      align-items: center;
-
+    align-items: center;
+    height: 100vh;
   }
 
+  #right-side {
+    height: 100%;
+    padding-top: 5vh;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  #right-side img {
+    max-width: 100%;
+    max-height: 95vh;
+  }
+
+  #left-side {
+    width: 50%;
+    align-content: right;
+    align-items: right;
+  }
+
+  #footer {
+    background-color: lightblue;
+    height: 70px;
+  }
+
+  button {
+    border: none;
+    color: white;
+  }
+
+  #lightpaper {
+    background-color: #AD0262;
+    padding: 18px 26px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    font-weight: 600;
+    border-radius: 4px;
+  }
+
+  #socials { 
+    margin-top: 10px;
+  }
+
+  #discord {
+    background: url(../assets/discord.png) no-repeat;
+    width: 50px;
+    height: 50px;
+    background-size: 100%;
+    margin: 10px;
+  } 
+
+  #telegram {
+    background: url(../assets/telegram.png) no-repeat;
+    width: 45px;
+    height: 45px;
+    background-size: 100%;
+    margin: 10px;
+  }
 </style>
